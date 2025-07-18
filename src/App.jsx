9d409c1a3a750f99ab46000d911c7303768c6956
angelo-lepore <angelo.lepore94@gmail.com>
2025-07-18 12:42:17 +0200

    Add the App component to manage and view Apple products

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,48 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "bootstrap-icons/font/bootstrap-icons.css";
 
+// arrays
+import AppleProducts from "./db/articoli";
+
 // App function
 function App() {
-  const [count, setCount] = useState(0);
+  const [AppleArray, setAppleArray] = useState(AppleProducts);
+  const [newAppleArray, setnewAppleArray] = useState("");
 
+  function handleSubmit(e) {
+    e.preventDefault();
+    setAppleArray([...AppleArray, newAppleArray]);
+    setnewAppleArray("");
+  }
   return (
     <>
       <div>
-        <h1>Pippo</h1>
+        <h1 className="text-center">Apple Products</h1>
+      </div>
+      <div className="container">
+        <ul className="list-group">
+          {AppleArray.map((product, index) => {
+            return (
+              <li key={index} className="list-group-item">
+                {product}
+              </li>
+            );
+          })}
+        </ul>
+        <form onSubmit={handleSubmit}>
+          <div className="d-flex">
+            <input
+              className="form-control mt-2"
+              placeholder="Insert new product"
+              type="text"
+              value={newAppleArray}
+              onChange={(event) => setnewAppleArray(event.target.value)}
+            />
+            <button type="submit" className="btn">
+              <i className="bi bi-save2">Save</i>
+            </button>
+          </div>
+        </form>
       </div>
     </>
   );
---
+++ src/db/articoli.js
@@ -0,0 +1,15 @@
+const AppleProducts = [
+  "iPhone 15 Pro",
+  "MacBook Air M2",
+  'MacBook Pro 14" M3',
+  "Apple Watch Series 9",
+  "Apple Watch Ultra 2",
+  'iPad Pro 12.9" M2',
+  'iPad Air 11" M2',
+  "AirPods Pro (2ª gen)",
+  "AirPods Max",
+  "Apple TV 4K",
+];
+
+// Export AppleProducts array
+export default AppleProducts;
